44a4cdde4009861f88d81093b0e832972e70e219
Chris Xiong <chrisxiongyang@yahoo.com>
2015-01-12 16:00:50 -0800

    set up Intake

--- src/org/usfirst/frc/team840/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team840/robot/subsystems/ExampleSubsystem.java
@@ -2,18 +2,36 @@
 package org.usfirst.frc.team840.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj;
 
 /**
  *
  */
-public class ExampleSubsystem extends Subsystem {
+public class Intake extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    private Talon lWheel;
+    private Talon rWheel;
+
+    public Intake() {
+        lWheel = new Talon(RobotMap.lWheel);
+        rWheel = new Talon(RobotMap.rWheel)
+    }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void lSpin(boolean dir) {
+        if (dir) lWheel.set(1);
+        else lWheel.set(-1);
+    }
+    public void rSpin(boolean dir) {
+        if (dir) rWheel.set(1);
+        else rWheel.set(-1);
     }
 }
 
